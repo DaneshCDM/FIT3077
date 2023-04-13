14f13b6a6acbc9ed2d45b728487c1396cae36863
Gan Jia Horng <jgan0022@student.monash.edu>
2023-04-13 21:53:45 +0800

    added javafx base files

---
+++ morris/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.nineman.morris {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.nineman.morris to javafx.fxml;
+    exports com.nineman.morris;
+}
