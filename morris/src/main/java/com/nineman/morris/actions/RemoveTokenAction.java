9da52058160e97351ceb162ef96b112321e4b4ef
dmar0038 <dmar0038@student.monash.edu>
2023-04-26 22:33:04 +0800

    JavaDoc for RemoveTokenAction.java

--- morris/src/main/java/com/nineman/morris/actions/RemoveTokenAction.java
+++ morris/src/main/java/com/nineman/morris/actions/RemoveTokenAction.java
@@ -3,7 +3,16 @@ package com.nineman.morris.actions;
 import com.nineman.morris.Board;
 import com.nineman.morris.Player;
 
+/**
+ * The RemoveTokenAction class represents an action to remove a token from the board.
+ */
 public class RemoveTokenAction implements Action {
+    /**
+     * Executes the remove token action on the provided player and board.
+     * @param player the player that is executing the action
+     * @param board the board on which the action is being executed
+     * @return true if the action was successfully executed, false otherwise
+     */
     @Override
     public boolean execute(Player player, Board board) {
         InputSource controller = player.getSource();
