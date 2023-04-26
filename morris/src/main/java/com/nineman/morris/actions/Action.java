cd8a1903cd3674894d4efb17c06c02b5e5dc0183
rrat0006 <rrat0006@student.monash.edu>
2023-04-26 22:17:13 +0800

    JavaDoc for Action and Input Source.java

--- morris/src/main/java/com/nineman/morris/actions/Action.java
+++ morris/src/main/java/com/nineman/morris/actions/Action.java
@@ -3,7 +3,16 @@ package com.nineman.morris.actions;
 import com.nineman.morris.Board;
 import com.nineman.morris.Player;
 
+/**
+ * Interface representing a game action in Nine Men's Morris.
+ */
 public interface Action {
 
+    /**
+     * Executes the action for a given player on a given board.
+     * @param player The player performing the action.
+     * @param board The game board on which the action is performed.
+     * @return true if the action is executed successfully, false otherwise.
+     */
     boolean execute(Player player, Board board);
 }
--- morris/src/main/java/com/nineman/morris/actions/InputSource.java
+++ morris/src/main/java/com/nineman/morris/actions/InputSource.java
@@ -1,5 +1,12 @@
 package com.nineman.morris.actions;
 
+/**
+ * Interface representing an input source for the Nine Men's Morris game actions.
+ */
 public interface InputSource {
+    /**
+     * Gets the input from the input source.
+     * @return The input as a string.
+     */
     String getInput();
 }
