80a96c1b733c38c490a7eb5cdb7ab94255fe2c4e
rrat0006 <rrat0006@student.monash.edu>
2023-04-26 20:38:47 +0800

    JavaDoc for GameController.java

--- morris/src/main/java/com/nineman/morris/GameController.java
+++ morris/src/main/java/com/nineman/morris/GameController.java
@@ -1,3 +1,11 @@
+/**
+ * GameController is the main controller for the 9 Man Morris game. It manages user interactions,
+ * updates the game state, and updates the game view. It implements the InputSource interface
+ * to process user input from the game view.
+ *
+ * @version 1.0
+ * @since 2023-04-26
+ */
 package com.nineman.morris;
 
 import com.nineman.morris.actions.InputSource;
@@ -22,6 +30,13 @@ public class GameController implements Initializable, InputSource {
     private ArrayBlockingQueue<String> clicks = new ArrayBlockingQueue<>(5);
     private final EnumMap<Color, String> COLOR_MAP = new EnumMap<>(Map.of(Color.WHITE, "wt", Color.BLACK, "bt"));
 
+    /**
+     * Initializes the GameController by setting up the game and user interactions.
+     * It sets up an executor to handle game state updates in response to user clicks.
+     *
+     * @param url The location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle The resources used to localize the root object, or null if the root object was not localized.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Game game = new Game(this);
@@ -40,6 +55,14 @@ public class GameController implements Initializable, InputSource {
         }
     }
 
+    /**
+     * Updates the game view based on the current state of the game. This method follows the
+     * Model-View-Controller (MVC) architecture by using the game model data to update the view.
+     * It clears and sets appropriate style classes for the token positions and updates the turn
+     * indicator text to reflect the current player's turn.
+     *
+     * @param game The current game state to be displayed, representing the model in the MVC architecture.
+     */
     public void update(Game game) {
         for (int i = 0; i < positions.getChildren().size(); i++) {
             Node tokenView = positions.getChildren().get(i);
@@ -54,6 +77,12 @@ public class GameController implements Initializable, InputSource {
 
     @FXML private Pane positions;
 
+    /**
+     * Retrieves user input in the form of clicks on the game board.
+     * This method blocks until input is available.
+     *
+     * @return A string representation of the clicked position's index.
+     */
     @Override
     public String getInput() {
         try {
