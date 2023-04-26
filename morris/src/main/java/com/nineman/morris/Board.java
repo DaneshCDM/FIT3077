da6c05e4a8d253162e0204c694be455a049c66fd
rrat0006 <rrat0006@student.monash.edu>
2023-04-26 21:27:19 +0800

    JavaDoc for Board.java

--- morris/src/main/java/com/nineman/morris/Board.java
+++ morris/src/main/java/com/nineman/morris/Board.java
@@ -7,11 +7,21 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Represents the Nine Men's Morris game board and manages its state.
+ * This class holds the positions and their connections, and provides methods
+ * for placing, moving, and removing tokens. It also detects mill formations
+ * and notifies listeners when a mill is formed.
+ */
 public class Board implements Iterable<Position> {
     private final Position[] positions;
     private List<MillListener> listeners;
     private int currentTurn;
 
+    /**
+     * Constructs a new Nine Men's Morris game board.
+     * Initializes the positions, their connections, and the listeners.
+     */
     public Board() {
         positions = new Position[24];
         for (int i = 0; i < positions.length; i++) {
@@ -32,6 +42,13 @@ public class Board implements Iterable<Position> {
         this.listeners = new ArrayList<>();
     }
 
+    /**
+     * Connects three given positions horizontally.
+     *
+     * @param left the leftmost position
+     * @param middle the middle position
+     * @param right the rightmost position
+     */
     private void connectHorizontal(Position left, Position middle, Position right) {
         left.setPositionRight(middle);
         middle.setPositionLeft(left);
@@ -39,6 +56,13 @@ public class Board implements Iterable<Position> {
         right.setPositionLeft(middle);
     }
 
+    /**
+     * Connects three given positions vertically.
+     *
+     * @param up the upper position
+     * @param middle the middle position
+     * @param down the lower position
+     */
     private void connectVertical(Position up, Position middle, Position down) {
         up.setPositionDown(middle);
         middle.setPositionUp(up);
@@ -46,12 +70,24 @@ public class Board implements Iterable<Position> {
         down.setPositionUp(middle);
     }
 
+    /**
+     * Checks if a mill is formed horizontally or vertically by the given color at the input position.
+     *
+     * @param i the input position
+     * @param color the color to check for a mill
+     * @return true if a mill is formed, false otherwise
+     */
     public boolean isPartOfMill(Position i, Color color) {
         return detectHorizontalMill(i, color) || detectVerticalMill(i, color);
     }
 
-    /** Detects if a mill is formed horizontally by color at input Position */
-    public boolean detectHorizontalMill(Position pos, Color color) {
+    /**
+     * Detects if a mill is formed horizontally by the given color at the input position.
+     *
+     * @param pos the input position
+     * @param color the color to check for a mill
+     * @return true if a mill is formed, false otherwise
+     */    public boolean detectHorizontalMill(Position pos, Color color) {
         Position left = pos.left();
         Position right = pos.right();
         if (left != null && right != null) {
@@ -64,8 +100,13 @@ public class Board implements Iterable<Position> {
         }
     }
 
-    /** Detects if a mill is formed vertically by color at input Position */
-    public boolean detectVerticalMill(Position pos, Color color) {
+    /**
+     * Detects if a mill is formed vertically by the given color at the input position.
+     *
+     * @param pos the input position
+     * @param color the color to check for a mill
+     * @return true if a mill is formed, false otherwise
+     */    public boolean detectVerticalMill(Position pos, Color color) {
         Position up = pos.up();
         Position down = pos.down();
         if (up != null && down != null) {
@@ -78,8 +119,14 @@ public class Board implements Iterable<Position> {
         }
     }
 
-    /** You can only place tokens on an empty position. Return value indicates if placement
-     * is successful. */
+    /**
+     * Places a token of the given color at the specified position.
+     * If the placement is successful and forms a mill, listeners are notified.
+     *
+     * @param position the position index to place the token
+     * @param color the color of the token to be placed
+     * @return true if placement is successful, false otherwise
+     */
     public boolean placeToken(int position, Color color) {
         if (positions[position].getColor() != null) {
             return false;
@@ -93,6 +140,13 @@ public class Board implements Iterable<Position> {
         return true;
     }
 
+    /**
+     * Moves a token from one position to another if the positions are adjacent.
+     *
+     * @param from the source position index
+     * @param destination the destination position index
+     * @return true if the token is moved successfully, false otherwise
+     */
     public boolean moveToken(int from, int destination) {
         if (positions[from].adjacent(positions[destination])) {
             return jumpToken(from, destination);
