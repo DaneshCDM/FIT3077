c317b1b05259975e22498f973f67955f4be619c4
rrat0006 <rrat0006@student.monash.edu>
2023-04-26 21:45:04 +0800

    JavaDoc for Player.java

--- morris/src/main/java/com/nineman/morris/Color.java
+++ morris/src/main/java/com/nineman/morris/Color.java
@@ -1,12 +1,24 @@
 package com.nineman.morris;
 
+/**
+ * Enum representing the two colors (White and Black) used for the tokens in a Nine Men's Morris game.
+ * Provides utility methods for inverting colors and obtaining player numbers.
+ */
 public enum Color {
     WHITE,
     BLACK;
+    /**
+     * Inverts the current color (switches between WHITE and BLACK).
+     * @return The inverted color.
+     */
     Color invert() {
         return values()[(ordinal() + 1) % values().length];
     }
 
+    /**
+     * Returns the player number corresponding to the color (1 for WHITE, 2 for BLACK).
+     * @return A string representing the player number.
+     */
     public String playerNumber() {
         return Integer.toString(ordinal() + 1);
     }
--- morris/src/main/java/com/nineman/morris/Position.java
+++ morris/src/main/java/com/nineman/morris/Position.java
@@ -1,5 +1,9 @@
 package com.nineman.morris;
 
+/**
+ * Class representing a position on the Nine Men's Morris game board.
+ * Manages the color of the token at the position and connections to adjacent positions.
+ */
 public class Position {
 
     /** The color of the token in this position. Null represents no token. */
@@ -9,50 +13,93 @@ public class Position {
     private Position positionUp;
     private Position positionDown;
 
-    /** Checks if input position is adjacent to this position. */
-    public boolean adjacent(Position pos) {
+    /**
+     * Checks if the input position is adjacent to this position.
+     * @param pos The position to check adjacency with.
+     * @return true if the input position is adjacent, false otherwise.
+     */    public boolean adjacent(Position pos) {
         if (pos == null) {
             return false;
         }
         return pos == positionLeft || pos == positionRight || pos == positionUp || pos == positionDown;
     }
 
+    /**
+     * Sets the left adjacent position of this position.
+     * @param positionLeft The position object to set as the left adjacent position.
+     */
     public void setPositionLeft(Position positionLeft) {
         this.positionLeft = positionLeft;
     }
 
+    /**
+     * Sets the right adjacent position of this position.
+     * @param positionRight The position object to set as the right adjacent position.
+     */
     public void setPositionRight(Position positionRight) {
         this.positionRight = positionRight;
     }
 
+    /**
+     * Sets the up adjacent position of this position.
+     * @param positionUp The position object to set as the up adjacent position.
+     */
     public void setPositionUp(Position positionUp) {
         this.positionUp = positionUp;
     }
 
+    /**
+     * Sets the down adjacent position of this position.
+     * @param positionDown The position object to set as the down adjacent position.
+     */
     public void setPositionDown(Position positionDown) {
         this.positionDown = positionDown;
     }
 
+    /**
+     * Gets the left adjacent position of this position.
+     * @return The left adjacent position.
+     */
     public Position left() {
         return positionLeft;
     }
 
+    /**
+     * Gets the right adjacent position of this position.
+     * @return The right adjacent position.
+     */
     public Position right() {
         return positionRight;
     }
 
+    /**
+     * Gets the up adjacent position of this position.
+     * @return The up adjacent position.
+     */
     public Position up() {
         return positionUp;
     }
 
+    /**
+     * Gets the down adjacent position of this position.
+     * @return The down adjacent position.
+     */
     public Position down() {
         return positionDown;
     }
 
+    /**
+     * Sets the color of the token at this position.
+     * @param color The color to set for the token at this position.
+     */
     public void setColor(Color color) {
         this.color = color;
     }
 
+    /**
+     * Gets the color of the token at this position.
+     * @return The color of the token at this position, or null if no token is present.
+     */
     public Color getColor() {
         return color;
     }
