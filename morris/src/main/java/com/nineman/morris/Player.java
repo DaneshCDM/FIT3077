6a5f91e39da41a4da8046dacbf4b42ac148bf782
rrat0006 <rrat0006@student.monash.edu>
2023-04-26 21:35:51 +0800

    JavaDoc for Player.java

--- morris/src/main/java/com/nineman/morris/Player.java
+++ morris/src/main/java/com/nineman/morris/Player.java
@@ -2,16 +2,32 @@ package com.nineman.morris;
 
 import com.nineman.morris.actions.InputSource;
 
+/**
+ * Represents a player in the Nine Men's Morris game.
+ * This class holds the player's color and input source.
+ * The input source is used to receive player input during the game.
+ */
 public class Player {
 
     private final InputSource source;
     public final Color color;
 
+    /**
+     * Constructs a new player with the given input source and color.
+     *
+     * @param source the input source to receive player input during the game
+     * @param color the color of the player's tokens
+     */
     public Player(InputSource source, Color color) {
         this.source = source;
         this.color = color;
     }
 
+    /**
+     * Returns the input source for the player.
+     *
+     * @return the input source for the player
+     */
     public InputSource getSource() {
         return source;
     }
