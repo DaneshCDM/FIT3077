3e357eeddd48a1a987b500c74f1d2370be2ebda9
dmar0038 <dmar0038@student.monash.edu>
2023-04-26 22:31:57 +0800

    JavaDoc for MoveTokenAction.java

--- morris/src/main/java/com/nineman/morris/actions/PlaceTokenAction.java
+++ morris/src/main/java/com/nineman/morris/actions/PlaceTokenAction.java
@@ -3,8 +3,16 @@ package com.nineman.morris.actions;
 import com.nineman.morris.Board;
 import com.nineman.morris.Player;
 
+/**
+ * The PlaceTokenAction class represents an action to place a token on the board.
+ */
 public class PlaceTokenAction implements Action {
-
+    /**
+     * Executes the place token action on the provided player and board.
+     * @param player the player that is executing the action
+     * @param board the board on which the action is being executed
+     * @return true if the action was successfully executed, false otherwise
+     */
     @Override
     public boolean execute(Player player, Board board) {
         InputSource controller = player.getSource();
