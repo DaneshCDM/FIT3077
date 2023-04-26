80d657af16ea31e4fdd2d194e592e4f5332e7d47
rrat0006 <rrat0006@student.monash.edu>
2023-04-26 20:32:58 +0800

    JavaDoc for MenuController.java

--- morris/src/main/java/com/nineman/morris/MenuController.java
+++ morris/src/main/java/com/nineman/morris/MenuController.java
@@ -1,3 +1,10 @@
+/**
+ * MenuController is responsible for managing the user interactions within the game menu.
+ * This class handles the event of switching from the menu scene to the game scene.
+ *
+ * @version 1.0
+ * @since 2023-04-26
+ */
 package com.nineman.morris;
 
 import javafx.event.ActionEvent;
@@ -12,6 +19,13 @@ import java.io.IOException;
 
 public class MenuController {
 
+    /**
+     * switchToGameScene method is triggered by an event (e.g., a button click) in the game menu.
+     * It loads the game-scene FXML layout file and switches the current scene to the game scene.
+     *
+     * @param event The event that triggered the method, typically a button click.
+     * @throws IOException If there is an issue loading the game-scene FXML layout file.
+     */
     @FXML
     protected void switchToGameScene(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("game-scene.fxml"));
