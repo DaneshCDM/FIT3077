62ba6cd07f568913361f5eb4271e715b7676c8ad
Gan Jia Horng <jgan0022@student.monash.edu>
2023-05-17 11:19:36 +0800

    shrink unused dependencies

--- morris/src/main/java/module-info.java
+++ morris/src/main/java/module-info.java
@@ -2,10 +2,6 @@ module com.nineman.morris {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
     requires annotations;
 
     opens com.nineman.morris to javafx.fxml;
