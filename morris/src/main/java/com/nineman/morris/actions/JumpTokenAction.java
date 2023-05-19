8248d77ce5ccbf98bf354d6ad2618a13b8446ce9
rrat0006 <rrat0006@student.monash.edu>
2023-05-19 18:01:48 +0800

    Jump Token Action Comments and code refactoring

--- morris/src/main/java/com/nineman/morris/actions/JumpTokenAction.java
+++ morris/src/main/java/com/nineman/morris/actions/JumpTokenAction.java
@@ -15,13 +15,18 @@ public class JumpTokenAction implements Action {
      */
     @Override
     public boolean execute(Player player, Board board) {
-        InputSource controller = player.getSource();
-        int position1, position2;
-        position1 = Integer.parseInt(controller.getInput());
+        InputSource controller = player.getSource();// Get the input source (presumably from the player)
+        int position1, position2;// Declare variables to store the positions for the token jump
+
+        position1 = Integer.parseInt(controller.getInput());// Read the first input and parse it as an integer
         if ((board.getPositions(position1).getColor() != player.color)) {
+            // Check if the token at position1 belongs to the current player
+            // If it doesn't, return false indicating that the action was not successfully executed
             return false;
         }
-        position2 = Integer.parseInt(controller.getInput());
+        position2 = Integer.parseInt(controller.getInput());// Read the second input and parse it as an integer
+        // Invoke the jumpToken method on the Board object, passing the positions as arguments
+        // Return the result of the jumpToken method as the result of the execute method
         return board.jumpToken(position1, position2);
     }
 }
