74a2ecac32e07155816cdcc0b8f82bf85727b19e
rrat0006 <rrat0006@student.monash.edu>
2023-05-19 14:38:36 +0800

    Move Toekn Action Comments and code refactoring

--- morris/src/main/java/com/nineman/morris/actions/MoveTokenAction.java
+++ morris/src/main/java/com/nineman/morris/actions/MoveTokenAction.java
@@ -15,13 +15,18 @@ public class MoveTokenAction implements Action {
      */
     @Override
     public boolean execute(Player player, Board board) {
+        // Get the input source (controller) associated with the player
         InputSource controller = player.getSource();
+        // Get the first position from the input source
         int position1, position2;
         position1 = Integer.parseInt(controller.getInput());
+        // Check if the color of the token at position1 is the same as the player's color
         if ((board.getPositions(position1).getColor() != player.color)) {
             return false;
         }
+        // Get the second position from the input source
         position2 = Integer.parseInt(controller.getInput());
+        // Move the token from position1 to position2 on the board
         return board.moveToken(position1, position2);
     }
 }
