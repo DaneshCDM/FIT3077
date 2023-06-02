6f7ef1a6c81f4ee4d514e0428074fe2ae9f5f5ee
Gan Jia Horng <jgan0022@student.monash.edu>
2023-06-02 14:43:30 +0800

    removed unused menuapp imports

--- morris/src/main/java/com/nineman/morris/MenuApplication.java
+++ morris/src/main/java/com/nineman/morris/MenuApplication.java
@@ -11,7 +11,6 @@ package com.nineman.morris;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
