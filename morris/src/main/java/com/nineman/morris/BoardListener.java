3f8246ff08a77ed765eb64a5681115a72575e222
dmar0038 <dmar0038@student.monash.edu>
2023-06-08 19:06:50 +0800

    Code Refactoring for BoardListener Interface Class: - onPositionSelected()

--- morris/src/main/java/com/nineman/morris/BoardListener.java
+++ morris/src/main/java/com/nineman/morris/BoardListener.java
@@ -4,12 +4,14 @@ package com.nineman.morris;
  * The BoardListener interface defines the callbacks for events related to the Nine Men's Morris game board.
  */
 public interface BoardListener {
+
     /**
      * Called when a mill is formed on the game board.
      * A mill is formed when three pieces of the same color are aligned in a row, column, or diagonal.
      * This callback can be used to notify listeners that a mill has been formed.
      */
     void onMillFormed();
+
     /**
      * Called when the game is over.
      *
@@ -17,5 +19,11 @@ public interface BoardListener {
      */
     void onGameOver(Color color);
 
+    /**
+     * Called when a position is selected on the game board.
+     *
+     * @param position The index of the selected position.
+     */
     void onPositionSelected(int position);
+
 }
